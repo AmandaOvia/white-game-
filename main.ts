b19f00cb141a0192e3be047e2945811fccbefd14
AmandaOvia <101339756+AmandaOvia@users.noreply.github.com>
2022-04-13 09:28:42 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,17 @@
 input.onButtonPressed(Button.A, function () {
     Sprite.move(-1)
 })
+input.onPinPressed(TouchPin.P2, function () {
+	
+})
 input.onButtonPressed(Button.B, function () {
     Sprite.move(1)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    game.pause()
 })
 input.onGesture(Gesture.Shake, function () {
 	
 })
 let Sprite: game.LedSprite = null
 Sprite = game.createSprite(2, 2)
-basic.forever(function () {
-	
-})
